07ca8ff9b29c1f0358bfcb456d3400bb0fc00660
R1sedge <MishaPolyakov04@mail.ru>
2025-08-25 18:20:31 +0300

    vcpkg correct test

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake .. -G "Visual Studio 17 2022" -A x64
+cmake -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake .. -G "Visual Studio 17 2022" -A x64
 cd ..
--- src/simulation/main.cpp
+++ src/simulation/main.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <magic_enum/magic_enum.hpp>
+
+enum class Color : int { RED = -10, BLUE = 0, GREEN = 10 };
+
 int main()
 {
+	std::cout << "Hello, World!!!\n";
+
+	Color c1 = Color::RED;
+	std::cout << magic_enum::enum_name(c1) << std::endl; // RED
+
 	return 0;
 }
