61e4ed1a4ce4e29ad7617b19db9c539cdab172ce
R1sedge <MishaPolyakov04@mail.ru>
2025-08-26 22:17:14 +0300

    1 particle with gravity and border collision detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,149 @@
-#include <iostream>
-#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "GLFW init failed!" << std::endl;
-        return -1;
+GLFWwindow* StartGLFW(); // Функция создания окна
+float screenHeight = 600.0f;
+float screenWidth = 800.0f;
+
+float centerX = screenWidth / 2.0f;
+float centerY = screenHeight / 2.0f;
+int res = 20;
+
+// Simulation constats
+float gravity = 9.81f;
+
+
+
+
+// Структура частицы
+struct Particle 
+{
+    float radius = 20.0f;
+    std::vector<float> position = { 0.f, 0.f };
+    std::vector<float> velocity = { 0.f, 0.f };
+};
+
+Particle particle1{ 20.f, {400.f, 580.f}, {0.f, 0.f} };
+
+void ApplyGravity(Particle& particle)
+{
+    particle.velocity[1] -= gravity / 100;
+    particle.position[0] += particle.velocity[0];
+    particle.position[1] += particle.velocity[1];
+}
+
+void CheckBorderCollision(Particle& particle)
+{
+    if (particle.position[1] < particle.radius) 
+    { 
+        particle.position[1] = 2 * particle.radius - particle.position[1];
+        particle.velocity[1] *= -0.95; 
+    }
+    else if (particle.position[1] > screenHeight - particle.radius)
+    {
+        particle.position[1] = 2 * (screenHeight - particle.radius) - particle.position[1];
+        particle.velocity[1] *= -0.95;
     }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+    if (particle.position[0] < particle.radius)
+    { 
+        particle.position[0] = 2 * particle.radius - particle.position[0];
+        particle.velocity[0] *=  -0.95;
+    }
+    else if (particle.position[0] > screenWidth - particle.radius)
+    {
+        particle.position[0] = 2 * (screenWidth - particle.radius) - particle.position[0];
+        particle.velocity[0] *= -0.95;
+    }
+}
+
+void DrawCircle(float cx, float cy, float radius, int res)
+{
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(cx, cy);
+
+    for (int i = 0; i <= res; i++) // <= res для закрытия круга
+    {
+        float angle = 2.0f * 3.1415926f * (float)i / res;
+        float x = cx + cos(angle) * radius;
+        float y = cy + sin(angle) * radius;
+        glVertex2f(x, y);
+    }
+    glEnd();
+}
+
+
+void DrawParticle(Particle& particle)
+{
+    DrawCircle(particle.position[0], particle.position[1], particle.radius, res = 20);
+}
+
+
+void SetupProjection()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, screenWidth, 0, screenHeight, -1, 1); // Устанавливаем ортографическую проекцию
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+
+
+
+int main()
+{
+    GLFWwindow* window = StartGLFW();
+    if (!window) return -1;
+
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "GLEW init failed!" << std::endl;
-        return -1;
+    // Настройка проекции
+    SetupProjection();
+
+ 
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        // Рисуем круг
+        glColor3f(0.0f, 0.0f, 0.0f);
+        DrawParticle(particle1);
+        
+        ApplyGravity(particle1);
+        CheckBorderCollision(particle1);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
     glfwTerminate();
     return 0;
+}
+
+
+
+
+GLFWwindow* StartGLFW()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return nullptr;
+    }
+
+    GLFWwindow* window = glfwCreateWindow((int)screenWidth, (int)screenHeight, "Fluid_simulation", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+
+    return window;
 }
